0e86168733df346c4802465375c8501d868113d4
Anurag <anuragkushwah134@gmail.com>
2022-12-04 16:23:37 +0530

    prime or not

---
+++ prime.js
@@ -0,0 +1,10 @@
+ sum=0
+    for (i=2; i<43; i++){
+        if (43%i==0){
+           sum+=1 
+        }
+    }if (sum<0){
+        console.log("No")
+    }else if (sum==0){
+        console.log("Yes")
+    }
